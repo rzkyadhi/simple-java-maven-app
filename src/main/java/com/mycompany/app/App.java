cc5fc2d5337f5c6a968c31374a949808fccde609
Ubuntu <ubuntukiki@Qwiqwi2VCPU4GRAM.neo.internal>
2023-11-11 13:03:56 +0700

    Add SOUT Second Try

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -12,6 +12,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println(MESSAGE);
         System.out.println("Trying Poll SCM");
+        System.out.println("Second Try");
     }
 
     public String getMessage() {
