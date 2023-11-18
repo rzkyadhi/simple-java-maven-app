0e051583570277c2dd09996a5721e0571315005f
Ubuntu <ubuntukiki@Qwiqwi2VCPU4GRAM.neo.internal>
2023-11-18 10:36:59 +0700

    Fixing import package on apptest

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,8 +1,8 @@
 package com.mycompany.app;
-
+import java.util.Random;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * Unit test for simple App.
